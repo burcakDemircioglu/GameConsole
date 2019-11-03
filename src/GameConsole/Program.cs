2f5afffb77d434082e2b4ae3a4f5802e3b81f68f
Burcak Kam <budemirc@microsoft.com>
2019-11-03 15:35:26 +0100

    Used null conditional operator

--- src/GameConsole/Program.cs
+++ src/GameConsole/Program.cs
@@ -6,12 +6,26 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
-            var player = new PlayerCharacter();
-            player.Name = "Burcak";
+            PlayerCharacter player = new PlayerCharacter();
             player.DaysSinceLastLogin = 42;
 
-            PlayerDisplayer.Write(player);
+            int days = player?.DaysSinceLastLogin ?? -1;
+            Console.WriteLine(days);
 
+            // PlayerDisplayer.Write(player);
+
+            PlayerCharacter[] players = new PlayerCharacter[3]
+            {
+               new PlayerCharacter{Name="Burcak"},
+               new PlayerCharacter(),
+               null
+            };
+
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
+
+            Console.WriteLine(p1 + " " + p2 + " " + p3);
             Console.ReadLine();
         }
     }
